2ba9ebce46866bc0aaae51a1634842e834de6c04
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-15 17:36:51 +0200

    Add header parser controller

--- server/controllers/parserControllers.js
+++ server/controllers/parserControllers.js
@@ -1,6 +1,13 @@
 exports.tsTest = async (req, res) => {
   try {
-    res.json("Hello API");
+    console.log(req.headers);
+    // need ipaddress, preferred language, software
+    let reply = {
+      ipaddress: req.ip,
+      language: req.get("accept-language"),
+      software: req.get("user-agent"),
+    };
+    res.json(reply);
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occurred" });
   }
