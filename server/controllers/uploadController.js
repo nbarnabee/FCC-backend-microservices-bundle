b4d199029d96c7cab5bb193d43d79068710a3135
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-17 14:29:37 +0200

    Implement file metadata service

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,5 +1,10 @@
 exports.uploadFile = async (req, res) => {
   try {
+    res.json({
+      name: req.files.upfile.name,
+      type: req.files.upfile.mimetype,
+      size: req.files.upfile.size,
+    });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occurred" });
   }
