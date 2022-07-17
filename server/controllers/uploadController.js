1f88fe7a488046cdeeb34b50c7e28bd30bc4c202
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-17 15:12:04 +0200

    Remove actual file upload option

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,21 +1,21 @@
 exports.uploadFile = async (req, res) => {
-  let newFile;
-  let uploadPath;
+  // let newFile;
+  // let uploadPath;
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send("No files were uploaded.");
   } else {
-    newFile = req.files.upfile;
-    uploadPath =
-      require("path").resolve("./") + "/public/uploads/" + newFile.name;
+    //   newFile = req.files.upfile;
+    //   uploadPath =
+    //     require("path").resolve("./") + "/public/uploads/" + newFile.name;
+    // }
+    // await newFile.mv(uploadPath, function (err) {
+    //   if (err) return res.status(500).send(err);
+    //   else {
+    res.json({
+      name: req.files.upfile.name,
+      type: req.files.upfile.mimetype,
+      size: req.files.upfile.size,
+    });
   }
-  await newFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err);
-    else {
-      res.json({
-        name: req.files.upfile.name,
-        type: req.files.upfile.mimetype,
-        size: req.files.upfile.size,
-      });
-    }
-  });
+  //   });
 };
