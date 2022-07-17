cb27c56989bced3180bb83279519738b57c025c4
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-17 15:35:35 +0200

    Try another approach

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -27,10 +27,11 @@ exports.uploadFile = async (req, res) => {
   if (!req.file || Object.keys(req.file).length === 0) {
     return res.status(400).send("No files were uploaded.");
   } else {
-    res.json({
+    response = {
       name: req.file.originalname,
       type: req.file.mimetype,
       size: req.file.size,
-    });
+    };
+    res.json(response);
   }
 };
