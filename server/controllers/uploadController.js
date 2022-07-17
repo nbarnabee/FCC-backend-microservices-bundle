0e42d396e1b05605b4f9e7a002a05580af32cd71
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-17 14:56:46 +0200

    Allow for actual file upload

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,11 +1,21 @@
 exports.uploadFile = async (req, res) => {
-  try {
-    res.json({
-      name: req.files.upfile.name,
-      type: req.files.upfile.mimetype,
-      size: req.files.upfile.size,
-    });
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occurred" });
+  let newFile;
+  let uploadPath;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  } else {
+    newFile = req.files.upfile;
+    uploadPath =
+      require("path").resolve("./") + "/public/uploads/" + newFile.name;
   }
+  await newFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+    else {
+      res.json({
+        name: req.files.upfile.name,
+        type: req.files.upfile.mimetype,
+        size: req.files.upfile.size,
+      });
+    }
+  });
 };
